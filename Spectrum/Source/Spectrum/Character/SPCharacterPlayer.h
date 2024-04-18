7997cf4a1c0fbcc1ab49facb0062aa6381ac6412
SEOK IN GYEONG <106717276+InGyeongSeok@users.noreply.github.com>
2024-04-18 16:57:47 +0900

    Update SPCharacterPlayer.h

--- Spectrum/Source/Spectrum/Character/SPCharacterPlayer.h
+++ Spectrum/Source/Spectrum/Character/SPCharacterPlayer.h
@@ -59,7 +59,7 @@ protected:
 	virtual void SetCharacterControlData(const class USPCharacterControlData* CharacterControlData);
 	void CameraMove();
 
-
+//
 
 	void ShoulderMove(const FInputActionValue& Value);
 	void ShoulderLook(const FInputActionValue& Value);
@@ -240,7 +240,7 @@ protected:
 
 protected:
 	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character")
-	//uint8 bIsAiming : 1; //���� 
+	//uint8 bIsAiming : 1; //     
 
 	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character")
 	//uint8 bIsHolding : 1; 
