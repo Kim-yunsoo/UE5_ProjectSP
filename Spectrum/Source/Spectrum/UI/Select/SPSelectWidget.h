ea36971c07d9cdeb0523d26b48912cf51c6523d6
YUN SOO KIM <118554050+Kim-yunsoo@users.noreply.github.com>
2024-05-23 15:15:13 +0900

    캐릭터 선택 위젯 만들기

---
+++ Spectrum/Source/Spectrum/UI/Select/SPSelectWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SPSelectWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPECTRUM_API USPSelectWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
