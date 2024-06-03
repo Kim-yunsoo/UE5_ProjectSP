9f6c3c583077a496929bf855e9feeb391d6f50d1
YUN SOO KIM <118554050+Kim-yunsoo@users.noreply.github.com>
2024-06-03 20:57:38 +0900

    HPWidget

--- Spectrum/Source/Spectrum/UI/HpBar/SPHpBarWidget.cpp
+++ Spectrum/Source/Spectrum/UI/HpBar/SPHpBarWidget.cpp
@@ -3,3 +3,24 @@
 
 #include "UI/HpBar/SPHpBarWidget.h"
 
+#include "Components/ProgressBar.h"
+
+USPHpBarWidget::USPHpBarWidget(const FObjectInitializer& ObjectInitializer) :Super(ObjectInitializer)
+{
+	MaxHp=-1.0f;
+}
+
+void USPHpBarWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	
+}
+
+void USPHpBarWidget::UpdateHpBar(float NewCurrentHp) 
+{
+	ensure(MaxHp > 0.0f);
+	if (HpProgressBar)
+	{
+		HpProgressBar->SetPercent(NewCurrentHp / MaxHp);
+	}
+}
--- Spectrum/Source/Spectrum/UI/HpBar/SPHpBarWidget.h
+++ Spectrum/Source/Spectrum/UI/HpBar/SPHpBarWidget.h
@@ -13,5 +13,21 @@ UCLASS()
 class SPECTRUM_API USPHpBarWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
+public:
+	USPHpBarWidget(const FObjectInitializer& ObjectInitializer);
+protected:
+	virtual void NativeConstruct() override;
+public:
+	FORCEINLINE void SetMaxHp(float NewMaxHp) { MaxHp = NewMaxHp; }
+	//위젯의 HP값을 변경해주기 위함
+	void UpdateHpBar(float NewCurrentHp);
+
+protected:
+	UPROPERTY()
+	TObjectPtr<class UProgressBar> HpProgressBar;
+
+	UPROPERTY()
+	float MaxHp;
 	
 };
